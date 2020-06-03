bff592fb7cd9baed79d05654bce6132898da9e38
Jessica Seo <jessicaseo83@gmail.com>
2020-06-03 17:51:34 +0000

    Connection module

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const net = require('net');
-
-// establish connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.148',
-    port: 50541
-  });
-  conn.setEncoding('utf8');
-
-  conn.on('connect', () => {
-    conn.write('Name: Hey');
-  });
-  
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  
-  return conn;
-};
 
 
+const { connect } = require('./client')
 console.log('Connecting...');
 connect();
+
