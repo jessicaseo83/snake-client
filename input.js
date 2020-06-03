0a517981f3da891ce477a4ca53e97616e82e7202
Jessica Seo <jessicaseo83@gmail.com>
2020-06-03 19:59:00 +0000

    Input Module

---
+++ input.js
@@ -0,0 +1,24 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function() {
+    stdin.on('data', (key) => {
+      // \u0003 maps to ctrl+c input
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  };
+
+  handleUserInput();
+  return stdin;
+}
+
+module.exports = { setupInput };
